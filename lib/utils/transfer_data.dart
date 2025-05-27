49bdc33118304967c3b8f8d913c502a06a228ce8
zaen-dotcom <143061650+zaen-dotcom@users.noreply.github.com>
2025-05-27 17:40:16 +0700

    transaksi

---
+++ lib/utils/transfer_data.dart
@@ -0,0 +1,17 @@
+final List<Map<String, String>> transferMethods = [
+  {
+    'name': 'DANA',
+    'rekening': '0812-3456-7890',
+    'atas_nama': 'MOHAMMAD FADIL ZAENI',
+  },
+  {
+    'name': 'BRI',
+    'rekening': '1234 5678 9012 3456',
+    'atas_nama': 'DIANAN ANDIN',
+  },
+  {
+    'name': 'BCA',
+    'rekening': '9876 5432 1098 7654',
+    'atas_nama': 'PT Golapak Mandiri',
+  },
+];
